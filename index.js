2ab8f8820ba59cf58e09c82693e7c218ac5a2e77
mujeeb <mujeebrahmanps01707@gmail.com>
2024-08-23 16:50:16 +0530

    User registration and login

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv')
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
-const app=express()
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-const port=3000
+const port = 3000
 
+// routes
+app.use('/api/users', userRoutes);
+
+//global error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({ status: "error", message: err.message });
+  });
+
+// connecting to database and starting server
 mongoose.connect(process.env.MONGODB)
- .then(
-    console.log("Database connected"),
-    app.listen(port,()=>{
-        console.log('Server connected on port '+port);
-    })
- )
+    .then(
+        console.log("Database connected"),
+        app.listen(port, () => {
+            console.log('Server connected on port ' + port);
+        })
+    )
